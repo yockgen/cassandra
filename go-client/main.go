8bc8f32a3391b197d6c60b3f33fc9cf09abeb0bb
Mah, Yock Gen <yock.gen.mah@intel.com>
2023-07-09 08:29:49 +0530

    Added golang cassandra example
    Signed-off-by: Mah, Yock Gen <yock.gen.mah@intel.com>

--- go-client/main.go
+++ go-client/main.go
@@ -1,42 +1,90 @@
 package main
 
 import (
-	"fmt"
-
+	"log"
+        "os"
+        "fmt"
 	"github.com/gocql/gocql"
+        "github.com/spf13/viper"
 )
 
-func main() {
-	cluster := gocql.NewCluster("10.233.8.211")
-	cluster.Port = 31631
-	cluster.Keyspace = "my_keyspace"
-	cluster.Consistency = gocql.Quorum
+// Execute the CQL query
+func executeQuery(session *gocql.Session, query string) error {
+	return session.Query(query).Exec()
+}
 
+func retrieveQuery(session *gocql.Session, query string) error {
+	// Execute the query
+	iter := session.Query(query).Iter()
+
+	// Get column names
+	columns := iter.Columns()
+
+	// Print column names
+	fmt.Println("Columns:")
+	for _, col := range columns {
+		fmt.Println(col)
+	}
+
+
+        var id string
+        var name string
+        var metric string
+        var host string
+
+        for iter.Scan(&id, &name, &metric, &host) {
+                fmt.Println("values:", id, name, metric, host)
+        }
+
+	// Check for any errors during iteration
+	if err := iter.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//example:
+//go run . "select id,name,metric,host from collection_config;"
+
+func main() {
+
+        viper.SetConfigFile("config.yaml")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Failed to read configuration file: %s", err)
+                return
+	}
+
+        host := viper.GetString("cassandra.host") + ":" + viper.GetString("cassandra.port")
+        username := viper.GetString("cassandra.username")
+	password := viper.GetString("cassandra.password")
+
+	// Create a cluster configuration
+	cluster := gocql.NewCluster(host)
+
+	// Set the keyspace to use
+	cluster.Keyspace = "telemetry" // Replace with your keyspace name
+
+	// Set other options like username, password, etc. if required
+        // Set authentication credentials
+	cluster.Authenticator = gocql.PasswordAuthenticator{
+		Username: username,
+		Password: password,
+	}
+
+
+	// Create a session
 	session, err := cluster.CreateSession()
 	if err != nil {
-		fmt.Println("Failed to connect:", err)
-		return
+		log.Fatal(err)
 	}
 	defer session.Close()
 
-        // Example query execution
-	query := session.Query("SELECT * FROM my_table")
-
-	// Execute the query
-	iter := query.Iter()
-	defer iter.Close()
-
-        var id string
-        var age int
-	var name string
-
-	for iter.Scan(&id,&age, &name) {
-		fmt.Println("ID:", id, "Name:", name, "Age:",age)
+	// Perform database operations here
+        query := os.Args[1] // Retrieve the query from command-line arguments
+	if err := retrieveQuery(session, query); err != nil {
+		log.Fatal(err)
 	}
+}
 
-	if err := iter.Close(); err != nil {
-		fmt.Println("Error:", err)
-	}
-
- }
 
